ad29417e545df5337dbf884d641c356be5b58726
davidhardson <davidhardson@hotmail.com>
2021-10-18 11:54:07 +0800

    汇虹首页定制

--- wuye/src/router/index.js
+++ wuye/src/router/index.js
@@ -255,7 +255,7 @@ const router= new VueRouter({
     ]
 });
 
-const viewArray = ['index', 'register', 'sms_notification', 'version2', 'version3']
+const viewArray = ['index', 'register', 'sms_notification', 'version2', 'version3','huihong']
 //路由的钩子函数，
 //在每一次路由跳转之前会进入这个方法 to：到哪去  from：从哪来 next() 调用这个方法来完成这个钩子函数
 router.beforeEach((to, from, next) => {
