68bdaf8c0113d3380ebd5879036398102859d2a2
davidhardson <davidhardson@hotmail.com>
2023-05-11 17:07:59 +0800

    调试

--- wuye/build/webpack.prod.conf.js
+++ wuye/build/webpack.prod.conf.js
@@ -35,8 +35,8 @@ const webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
-        drop_debugger:true,
-        drop_console:true
+        drop_debugger:false,
+        drop_console:false
       },
       mangle: {
         safari10: true
